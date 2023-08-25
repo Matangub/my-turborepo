bfe0da1141f0b38f068249464f8c8c5c0de06c33
matangubkin <Matangub@gmail.com>
2023-08-26 01:20:21 +0300

    added analytics

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	import '../app.postcss';
+  import '../app.postcss';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  // Inject the Analytics functionality
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
-	<title>Username &amp; password auth with Lucia</title>
+  <title>Username &amp; password auth with Lucia</title>
 </svelte:head>
 <slot />
